d28c32718ba49060db7e5db13800c00b2e18d3d9
Daniel Aros Ramírez <danielaros200@gmail.com>
2022-09-23 12:19:42 -0500

    modal de generacion QR creado

---
+++ src/Components/Modals/ModalQR/ModalQR.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import "./ModalQR.css";
+
+import QRCode from "react-qr-code";
+import indereqIcon from "../../../Assets/icons/indereq-icon.png";
+import close from "../../../Assets/icons/close.png";
+
+/**
+ * @param datos dentro del componente se pasa un objeto con el id y el nombre.
+ * @ejemplo datos={{id: '1', nombre: 'Daniel Aros Ramirez'}}
+ *  
+*/
+
+const ModalQR = (props) => {
+    return (
+        <div className="container-modal">
+            <div className="container-header">
+                 <p className="title">Información guardada correctamente</p>
+                <img src={close} id="close"/>
+            </div>
+            <div className="container-info">
+                <div className="container-img-qr">
+                    <QRCode value={`${JSON.stringify(props.datos)}`} level='H' className="qr"/>
+                    <div className="container-indereq-icon">
+                        <img src={indereqIcon} className="indereq-icon"/>
+                    </div>
+                </div>
+                <p className="info">QR Generado.</p>
+                <p className="info">No olvides escanearlo en tu celular.</p>
+            </div>
+            <div className="container-button">
+                <button id="button-salir">Salir</button>
+            </div>
+        </div>
+    )
+}
+
+export default ModalQR;
+
