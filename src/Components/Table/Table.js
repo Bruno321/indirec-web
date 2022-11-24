dacb86695b4e4fa8369841278eff8db8fd85a8ce
Jonathan Jesús Herrera López <jonathan_herrera117@hotmail.com>
2022-11-24 10:39:01 -0600

    Modified table component to make it dynamic
    Using columns array, similar to the list component in mobile

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -1,144 +1,49 @@
 import React from "react";
 import "./Table.css";
 
-import iconDelete from "../../Assets/icons/delete.png";
-import iconEdit from "../../Assets/icons/edit.png";
-import iconMoreInfo from "../../Assets/icons/more-info.png";
-
-import MoreInfo from "../MoreInfo/MoreInfo.js"
-
 import moment from "moment/moment";
-import { useState } from "react";
 moment.locale('es');
-/**
- * @param datos Se recibe como un arreglo de objetos.
- * @param tipo El tipo de datos especifica a la tabla que campos se renderizan. Los parametros son 'asistencias' o 'deportistas'
- * @ejemplo para asistencias let datos = [
-        {
-            nombre: 'Daniel',
-            apellidos: 'Aros Ramirez',
-            fecha: '21/09/2022',
-            horaEntrada: '10:48',
-            horaSalida: '13:52'
-        },
-    ]
- * @ejemplo para deportistas let datos = [
-        {
-            expediente: '290246',
-            nombre: 'Daniel',
-            apellidos: 'Aros Ramirez',
-            facultad: 'informatica',
-            correo: 'danielaros200@gmail.com',
-            telefono: '4681121267',
-            telefonoEmergencia: '6881583',
-            noJugador: '2'
-        },
-    ]
-*/
 
-const Table = (props) => {
-    const {datos, tipo} = props;
-    // console.log(datos)
+const handleRender = valueToRender => {
+    const type = typeof valueToRender;
 
+    if (type === 'string' || type === 'number') {
+        return <td className="td-font-weight td">{valueToRender}</td>;
+    }
+    return valueToRender;
+};
 
-    //State para mostrar MAS INFORMACION de un deportista
-    const [buttonMoreInfo, setButtonMoreInfo] = useState(false);
-    const [infoJugador, setInfoJudador] = useState();
+export const Table = ({ dataSource, columns, loading }) => {    
 
-    return (
-        <div className="container">
-            {
-                tipo == 'deportistas'
-                ?
-                    <h3>Deportistas</h3>
-                :
-                    <h3>Asistencias</h3>
-            }
-            <table id="data">
-                <tbody>
-                    <tr className="header">
-                        {
-                            tipo == 'deportistas'
-                            ?   
-                                <>
-                                    <th className="th">Expediente</th>
-                                    <th className="th">Nombre(s)</th>
-                                    <th className="th">Apellido(s)</th>
-                                    <th className="th">Facultad</th>
-                                    <th className="th">Correo</th>
-                                    <th className="th">Teléfono</th>
-                                    <th className="th">Tel. Emergencias</th>
-                                    <th className="th">No. Jugador</th>
-                                    <th className="th">Opciones</th>
-                                </>
-                            :
-                                <>
-                                    <th className="th">Nombre(s)</th>
-                                    <th className="th">Apellido(s)</th>
-                                    <th className="th">Fecha</th>
-                                    <th className="th">Hora de entrada</th>
-                                    <th className="th">Hora de salida</th>
-                                </>
-                        }
-                    </tr>
-                    {datos.length===0  
-                    ? 
-                    <div>No hay datos para mostrar</div>
-                    :
-                    <>
-                        {
-                        tipo == 'deportistas'
-                        ?   
-                        datos.map( (element, index) => 
-                        <tr key={index}>
-                            <td className = "td-font-weight td">{element.expediente}</td>
-                            <td className = "td-font-weight td">{element.nombres}</td>
-                            <td className = "td-font-weight td">{element.apellidos}</td>
-                            <td className = "td-font-weight td">{element.facultad}</td>
-                            <td className = "td-font-weight td">{element.correo}</td>
-                            <td className = "td-font-weight td">{element.telefono}</td>
-                            <td className = "td-font-weight td">{element.telefonoEmergencia}</td>
-                            <td className = "td-font-weight td">{element.numJugador}</td>
-                            <td className="container-edits td"> 
-                                <img title="Editar" src={iconEdit} className='icons edit'/>
-                                <img title="Eliminar" src={iconDelete} className='icons delete'/>
-                                <img title="Más información" src={iconMoreInfo} className='icons moreinfo' onClick={()=> {setButtonMoreInfo(true); setInfoJudador(index)}}/>
-                            </td>
+    return loading ? (
+    // TODO: Add spinner
+    <p>Cargando</p>
+    ) : (
+        dataSource?.length ? (
+            <div className="container">
+                <table id="data">
+                    <tbody>
+                        <tr className="header">
+                            {columns.map(oColumn => (
+                                <th className="th">{oColumn.title}</th>
+                            ))}
                         </tr>
-                    )
-                        :
-                            // datos.map( (element, index) => 
-                            //     <tr key={index}>
-                            //         <td>{element.nombre}</td>
-                            //         <td>{element.apellidos}</td>
-                            //         <td>{moment(element.fecha).format("dddd, MMMM D, YYYY")}</td>
-                            //         <td>{moment(element.horaEntrada).format("h:mm a")}</td>
-                            //         <td>{moment(element.horaSalida).format("h:mm a")}</td>
-                            //     </tr>
-                            // )
-                            datos.map( (element, index) => {
-                                return (
-                                    <>
-                                    <tr key={index} >
-                                        <td>{element.deportistum ? element.deportistum.nombres : null}</td>
-                                        <td>{element.deportistum ? element.deportistum.apellidos : null}</td>
-                                        <td>{moment(element.fecha).format("dddd, MMMM D, YYYY")}</td>
-                                        <td>{moment(element.horaEntrada).format("h:mm a")}</td>
-                                        <td>{moment(element.horaSalida).format("h:mm a")}</td>
-                                    </tr>
-                                    </>
-                                )
-                            }
-                            )
-                    }
-                    </>
-                    }
-                    
-                </tbody>
-            </table>
-            <MoreInfo trigger={buttonMoreInfo} setTrigger={setButtonMoreInfo} datos={datos[infoJugador]}></MoreInfo>
-        </div>
-    )
-}
-
-export default Table;
+                        {dataSource.map((oData, index) => (
+                            <tr key={index}>
+                                {columns.map(({ dataIndex, render }) => render === undefined ? (
+                                    <td className="td-font-weight td">{oData[dataIndex]}</td>
+                                ) : (
+                                    <td className="container-edits td">
+                                        {handleRender(render(oData[dataIndex], oData, index))}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        ) : (
+            <div>No hay datos para mostrar</div>
+        )
+    );
+};
